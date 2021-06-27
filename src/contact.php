1d408bfd5a78eaaa72b205c24ce613eaad309b3e
Kevin BERTAUX <58264753+KevinBertaux@users.noreply.github.com>
2021-06-27 19:02:26 +0200

    Début du formulaire de contact

--- src/contact.php
+++ src/contact.php
@@ -1,2 +1,38 @@
-<?php $page='contact'; 
+<?php $page = 'contact';
 $title = 'Contactez-nous'; ?>
+
+<?php include('php/header.php'); ?>
+
+<section class="contact" id="contact">
+	<div class="container-narrow">
+
+		<h1 class="contact-title h2-like">Nous contacter</h1>
+
+		<form action="" method="post">
+			<ul class="field-list" role="list">
+				<li class="field-item">
+					<label for="name">Votre nom</label>
+					<input type="text" id="name" name="user_name" aria-label="Votre nom" placeholder="Entrez votre nom">
+				</li>
+				<li class="field-item">
+					<label for="mail">Votre e-mail</label>
+					<input type="email" id="mail" name="user_mail" aria-label="Votre e-mail" placeholder="Entrez votre adresse e-mail">
+				</li>
+				<li class="field-item">
+					<label for="msg">Votre message</label>
+					<textarea id="msg" name="user_message" aria-label="Votre message" placeholder="Tapez votre message"></textarea>
+				</li>
+			</ul>
+
+			<button class="contact-button btn">
+				<p>Envoyer</p>
+				<img src="img/ico_arrow_right.svg" alt="Une icone de flèche vers la droite">
+			</button>
+
+		</form>
+	</div>
+</section>
+
+
+
+<?php include('php/footer.php'); ?>
