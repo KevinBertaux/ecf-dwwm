6229692407a6e5573ace353e9b739c98b6dd5436
Kevin BERTAUX <58264753+KevinBertaux@users.noreply.github.com>
2021-06-27 21:16:46 +0200

    Ajout de send_contact.php et ico_shredder.svg

---
+++ dist/send_contact.php
@@ -0,0 +1,17 @@
+<?php $page = 'contact';
+$title = 'Contactez-nous'; ?>
+
+<?php include('php/header.php'); ?>
+
+<section class="send_contact" id="send_contact">
+	<div class="container-narrow">
+
+		<h1 class="contact-title h2-like">Merci !</h1>
+		<p>Votre message va bien être "traité".</p>
+		<img src="img/ico_shredder.svg" alt="Icone d'un destructeur de documents">
+	</div>
+</section>
+
+
+
+<?php include('php/footer.php'); ?>
