c4f8e8d464e9e3f79ebf04ae14d4e171d8a0fbf3
Kevin BERTAUX <58264753+KevinBertaux@users.noreply.github.com>
2021-07-01 22:24:00 +0200

    Début de la landing page

--- src/index.php
+++ src/index.php
@@ -2,7 +2,18 @@
 
 <?php include('php/header.php'); ?>
 
-
+<section id="hero" class="hero">
+	<div class="container">
+		<div class="title-card">
+			<h1 class="hero-title">Bienvenue sur le site de la <br>promotion DWWM2021-3</h1>
+			<ul class="cta-list" role="list">
+				<li class="cta-item">
+					<a href="students.php" class="cta-link btn">Rencontrez nos étudiants</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</section>
 
 
 
