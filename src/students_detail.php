f92f5f319e4688fdcfb2c89b524739e2fb83d44f
Kevin BERTAUX <58264753+KevinBertaux@users.noreply.github.com>
2021-07-19 21:19:23 +0200

    Page students detail

--- src/students_detail.php
+++ src/students_detail.php
@@ -3,6 +3,53 @@ $title = 'Étudiants'; ?>
 
 <?php include('php/header.php'); ?>
 
-
+<section class="student-detail-section">
+	<div class="container">
+		<div class="student-detail-main">
+			<img src="img/ava_francine_large.webp" alt="Photo de Francine Auhi" class="student-detail-image">
+			<h1 class="student-detail-name">Francine Auhi</h1>
+		</div>
+		<div class="student-detail-detail">
+			<ul class="student-detail-list" role="list">
+				<li class="student-detail-item">
+					<h2 class="student-detail-item-title h3-like">Son film</h2>
+					<p class="student-detail-item-desc">Lorem ipsum dolor sit</p>
+				</li>
+				<li class="student-detail-item">
+					<h2 class="student-detail-item-title h3-like">Sa série</h2>
+					<p class="student-detail-item-desc">Lorem ipsum dolor sit</p>
+				</li>
+				<li class="student-detail-item">
+					<h2 class="student-detail-item-title h3-like">Son jeu vidéo</h2>
+					<p class="student-detail-item-desc">Lorem ipsum dolor sit</p>
+				</li>
+				<li class="student-detail-item">
+					<h2 class="student-detail-item-title h3-like">Son héros/héroïne</h2>
+					<p class="student-detail-item-desc">Lorem ipsum dolor sit</p>
+				</li>
+				<li class="student-detail-item">
+					<h2 class="student-detail-item-title h3-like">Son livre</h2>
+					<p class="student-detail-item-desc">Lorem ipsum dolor sit</p>
+				</li>
+				<li class="student-detail-item">
+					<h2 class="student-detail-item-title h3-like">Sa chanson</h2>
+					<p class="student-detail-item-desc">Lorem ipsum dolor sit</p>
+				</li>
+				<li class="student-detail-item">
+					<h2 class="student-detail-item-title h3-like">Son application</h2>
+					<p class="student-detail-item-desc">Lorem ipsum dolor sit</p>
+				</li>
+				<li class="student-detail-item">
+					<h2 class="student-detail-item-title h3-like">Son site web</h2>
+					<p class="student-detail-item-desc">Lorem ipsum dolor sit</p>
+				</li>
+				<li class="student-detail-item">
+					<h2 class="student-detail-item-title h3-like">En deux mots</h2>
+					<p class="student-detail-item-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis. Iaculis urna id volutpat lacus laoreet.</p>
+				</li>
+			</ul>
+		</div>
+	</div>
+</section>
 
 <?php include('php/footer.php'); ?>
