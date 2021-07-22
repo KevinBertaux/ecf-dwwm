438c8202f93ad3ec68d233e522bc757929aa77e9
Kevin BERTAUX <58264753+KevinBertaux@users.noreply.github.com>
2021-07-22 21:45:01 +0200

    Fin du HTML CSS avant W3C Validatior et responsive

--- src/formation_detail.php
+++ src/formation_detail.php
@@ -3,6 +3,16 @@ $title = 'Formation'; ?>
 
 <?php include('php/header.php'); ?>
 
+<section class="formation-detail-title-section">
+	<div class="container-narrower">
+		<h1 class="formation-detail-title">Module HTML / CSS</h1>
+	</div>
+</section>
 
+<section class="formation-detail-desc-section">
+	<div class="container-narrower">
+		<p class="formation-detail-desc h3-like">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis. Iaculis urna id volutpat lacus laoreet. Mauris vitae ultricies leo integer malesuada. Ac odio tempor orci dapibus ultrices in. Egestas diam in arcu cursus euismod. Dictum fusce ut placerat orci nulla. Tincidunt ornare massa eget egestas purus viverra accumsan in nisl. Tempor id eu nisl nunc mi ipsum faucibus. Fusce id velit ut tortor pretium. Massa ultricies mi quis hendrerit dolor magna eget. Nullam eget felis eget nunc lobortis. Faucibus ornare suspendisse sed nisi. Sagittis eu volutpat odio facilisis mauris sit amet massa. Erat velit scelerisque in dictum non consectetur a erat. Amet nulla facilisi morbi tempus iaculis urna. Egestas</p>
+	</div>
+</section>
 
 <?php include('php/footer.php'); ?>
