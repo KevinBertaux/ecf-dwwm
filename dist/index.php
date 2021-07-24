92acb9ea0a79ed704f82af193ee29416c3c522be
Kevin BERTAUX <58264753+KevinBertaux@users.noreply.github.com>
2021-07-24 10:56:45 +0200

    CTA responsive

--- dist/index.php
+++ dist/index.php
@@ -5,8 +5,8 @@ $title = 'Accueil'; ?>
 
 <section id="hero" class="hero">
 	<div class="container">
-		<div class="title-card">
-			<h1 class="hero-title">Bienvenue sur le site de la <br>promotion DWWM2021-3</h1>
+		<div class="cta-card">
+			<h1 class="cta-title">Bienvenue sur le site de la <br>promotion DWWM2021-3</h1>
 			<ul class="cta-list" role="list">
 				<li class="cta-item">
 					<a href="students.php" class="cta-link btn">Rencontrez nos étudiants</a>
